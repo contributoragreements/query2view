f60cced1fc036a75ad4dca20527b57f35f96e2e2
Jon Phillips <jon@fabricatorz.com>
2014-11-04 13:27:38 +0800

    fixed up

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
  *
  */
 
+include_once '../lib/config.php';
+include_once '../lib/utils.php';
 include_once '../lib/shared.php';
 include_once '../lib/file.php';
 include_once '../lib/file-updates.php';
